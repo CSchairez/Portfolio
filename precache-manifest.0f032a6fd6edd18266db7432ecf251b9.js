db16db007e4fb5ac93659897dc4b2ff1f3e92a56
Michael Chairez <mchairez@horizon.csueastbay.edu>
2020-03-28 15:58:28 -0700

    Updates

---
+++ precache-manifest.0f032a6fd6edd18266db7432ecf251b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083ef507885f841b5ddcd3e33e9ce21f",
+    "url": "/CSchairez.github.io/index.html"
+  },
+  {
+    "revision": "360bffab7ec9412a24b1",
+    "url": "/CSchairez.github.io/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "d5c52f2b2b462c33ab59",
+    "url": "/CSchairez.github.io/static/css/main.210cccd4.chunk.css"
+  },
+  {
+    "revision": "360bffab7ec9412a24b1",
+    "url": "/CSchairez.github.io/static/js/2.e934ec4d.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/CSchairez.github.io/static/js/2.e934ec4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5c52f2b2b462c33ab59",
+    "url": "/CSchairez.github.io/static/js/main.0b353be3.chunk.js"
+  },
+  {
+    "revision": "52dba4086d4976ff68ec",
+    "url": "/CSchairez.github.io/static/js/runtime-main.3a85116c.js"
+  },
+  {
+    "revision": "b06f94d41da8daa11de45c0685499325",
+    "url": "/CSchairez.github.io/static/media/myAvatar.b06f94d4.png"
+  }
+]);
